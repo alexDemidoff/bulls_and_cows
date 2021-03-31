8887e1deb1e748e291f86a02e34a6714f14dac29
alexDemidoff <alexdeveloper123@gmail.com>
2021-03-31 16:05:23 +0300

    Stage 1 completed
    Predifined game log

--- bullscows/Main.java
+++ bullscows/Main.java
@@ -2,6 +2,18 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int secretCode = 1234;
+
+        System.out.println("The secret code is prepared: ****.");
+        System.out.println();
+
+        System.out.println("Turn 1. Answer:");
+        System.out.println("5374");
+        System.out.println("Grade: 1 bull and 1 cow.");
+
+        System.out.println("Turn 2. Answer:");
+        System.out.println("1234");
+        System.out.println("Grade: 4 bulls.");
+        System.out.println("Congrats! The secret code is " + secretCode + ".");
     }
 }
